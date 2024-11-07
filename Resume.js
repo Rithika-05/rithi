83a083c2c336233806b7c77b7f737020b54cd996
Rithika-05 <rithikavenkatesh99@gmail.com>
2024-11-07 19:25:01 +0530

    added Resume.js

---
+++ Resume.js
@@ -0,0 +1,4 @@
+// Example of dynamic content addition
+document.getElementById('name').innerHTML = 'Rithika B';
+document.getElementById('about-me').innerHTML = 'A passionate developer skilled in building scalable web applications.';
+document.getElementById('powered').innerHTML = 'Rithika\'s Professional Resume';
